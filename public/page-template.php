43f65e642cc51dd732979587bd944181ae027108
jambonbill <jambonbill@gmail.com>
2023-03-09 14:54:02 +0100

    template

---
+++ public/page-template.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Telethon - Page template</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/telethon.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+</head>
+<body>
+    
+<?php
+require_once("header.html");
+?>
+
+<main class="container">
+    
+    <div class="row">
+        <div class="col-12">
+            <h2><i class="fa-solid fa-circle-user"></i>Le titre en h2</h2>
+        </div>
+
+        <div class="col-12">            
+            
+            bla bla bla bla
+
+        </div>
+
+    </div>
+    
+</main>
+
+<?php
+require_once("footer.html");
+?>
+</body>
+</html>

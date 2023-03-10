ac7ef2b8e40442b08cf765990bdc408831c1f31c
Liszto <thjerem@gmail.com>
2023-03-10 08:32:44 +0100

    Pages collectes telethon2

---
+++ public/collect_main.php
@@ -0,0 +1,104 @@
+<?php
+require_once("head.php");
+require_once("header.php");
+?>
+<div class="d-flex container-md text-primary py-4 collectes ">
+
+    <div class=" titre">
+        <h2><i class=" fa-solid fa-hand-holding-dollar "></i> Collectes</h2>
+    </div>
+
+</div>
+<div class="container-md bouton pb-4">
+    <a class="btn btn-success px-md-5" href="collect_add.php">Ajouter</a>
+</div>
+<div class="container-md dons">
+    <table class="table table-striped table-hover align-middle ">
+
+        <tr class="">
+            <th> Auchan 1 </td>
+            <td> 12 Decembre 14h63</td>
+            <td>25 Euros</td>
+            <td>
+                <a class="btn btn-success float-end" href="collect_edit.php"><i class="fa-solid fa-pencil"></i></a>
+            </td>
+            <td>
+                <a class=" btn float-end btn-light" href=""><i class="fa-solid fa-x text-danger"></i></a>
+            </td>
+        </tr>
+        <tr class="">
+            <th> Auchan 1 </td>
+            <td> 12 Decembre 14h63</td>
+            <td>25 Euros</td>
+            <td>
+                <a class="btn btn-success float-end" href="collect_edit.php"><i class="fa-solid fa-pencil"></i></a>
+            </td>
+            <td>
+                <a class="btn float-end btn-light" href=""><i class="fa-solid fa-x text-danger"></i></a>
+            </td>
+        </tr>
+        <tr class="">
+            <th> Auchan 1 </td>
+            <td> 12 Decembre 14h63</td>
+            <td>25 Euros</td>
+            <td>
+                <a class="btn btn-success float-end" href="collect_edit.php"><i class="fa-solid fa-pencil"></i></a>
+            </td>
+            <td>
+                <a class="btn float-end btn-light" href=""><i class="fa-solid fa-x text-danger"></i></a>
+            </td>
+        </tr>
+        <tr class="">
+            <th> Auchan 1 </td>
+            <td> 12 Decembre 14h63</td>
+            <td>25 Euros</td>
+            <td>
+                <a class="btn btn-success float-end" href="collect_edit.php"><i class="fa-solid fa-pencil"></i></a>
+            </td>
+            <td>
+                <a class="btn float-end btn-light" href=""><i class="fa-solid fa-x text-danger"></i></a>
+            </td>
+        </tr>
+        <tr class="">
+            <th> Auchan 1 </td>
+            <td> 12 Decembre 14h63</td>
+            <td>25 Euros</td>
+            <td>
+                <a class="btn btn-success float-end" href="collect_edit.php"><i class="fa-solid fa-pencil"></i></a>
+            </td>
+            <td>
+                <a class="btn float-end btn-light" href=""><i class="fa-solid fa-x text-danger"></i></a>
+            </td>
+        </tr>
+        <tr class="">
+            <th> Auchan 1 </td>
+            <td> 12 Decembre 14h63</td>
+            <td>25 Euros</td>
+            <td>
+                <a class="btn btn-success float-end" href="collect_edit.php"><i class="fa-solid fa-pencil"></i></a>
+            </td>
+            <td>
+                <a class="btn float-end btn-light" href=""><i class="fa-solid fa-x text-danger"></i></a>
+            </td>
+        </tr>
+
+    </table>
+</div>
+
+
+</body>
+
+<?php
+require_once("footer.php");
+?>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+
+</body>
+
+</html>

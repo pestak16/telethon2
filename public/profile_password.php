33be93712f2adb7ce847abf4fc004aed00d32693
jambonbill <jambonbill@gmail.com>
2023-03-10 10:30:25 +0100

    required

--- public/profile_password.php
+++ public/profile_password.php
@@ -16,13 +16,13 @@ require_once("header.php");
                 
               <div class="mb-3">
                   <label for="password1" class="form-label">Nouveau mot de passe</label>
-                  <input type="password" class="form-control" id="password1" placeholder="mot de passe">
+                  <input type="password" class="form-control" id="password1" placeholder="mot de passe" required>
                   <div id="emailHelp" class="form-text"><i class="text-muted">Au moins 12 chiffres, 3 majuscules, et 9 symboles grecs</i></div>
                 </div>
             
                 <div class="mb-3">
                   <label for="password2" class="form-label">R&eacute;p&eacute;ter le mot de passe</label>
-                  <input type="password" class="form-control" id="password2" placeholder="mot de passe">
+                  <input type="password" class="form-control" id="password2" placeholder="mot de passe" required>
                 </div>
                 
                 <button type="submit" class="btn btn-success">Changer le mot de passe</button>

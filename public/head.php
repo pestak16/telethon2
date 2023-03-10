2a6ad32ea6af7500f01deb86a7975bfc1d5ca0d2
jambonbill <jambonbill@gmail.com>
2023-03-10 11:33:04 +0100

    <link rel="shortcut icon" href="img/mascotte_babouche.svg" type="image/svg">

--- public/head.php
+++ public/head.php
@@ -8,5 +8,6 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/telethon.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="shortcut icon" href="img/mascotte_babouche.svg" type="image/svg">
 </head>
 <body>
